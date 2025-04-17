dcde0fa16439a17b52b4d5eca4d18deb5d5e7a0b
fredericorbgs <fredericorborges@outlook.com>
2025-04-17 02:49:53 -0300

    Update route.ts

--- app/api/files/route.ts
+++ app/api/files/route.ts
@@ -5,13 +5,13 @@ import {
   type _Object
 } from '@aws-sdk/client-s3'
 
-const { AWS_REGION, S3_BUCKET_NAME } = process.env
+const { REGION, S3_BUCKET_NAME } = process.env
 
-if (!AWS_REGION || !S3_BUCKET_NAME) {
-  throw new Error('Defina AWS_REGION e S3_BUCKET_NAME nas env vars.')
+if (!REGION || !S3_BUCKET_NAME) {
+  throw new Error('Defina REGION e S3_BUCKET_NAME nas env vars.')
 }
 
-const s3 = new S3Client({ region: AWS_REGION })
+const s3 = new S3Client({ region: REGION })
 
 export async function GET() {
   const { Contents = [] } = await s3.send(
