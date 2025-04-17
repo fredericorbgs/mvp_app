dc02773d9d41fca1546021cf8edb9c94c44b8d1d
fredericorbgs <fredericorborges@outlook.com>
2025-04-17 02:21:53 -0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    // ⚠️ faz o build não falhar por erros de ESLint
-    ignoreDuringBuilds: true,
-  },
-};
-
-export default nextConfig;
