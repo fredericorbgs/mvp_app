22dfdad2ae6ebec14f6af8dc77e409e8390d5129
fredericorbgs <fredericorborges@outlook.com>
2025-04-22 15:43:02 -0300

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},    // se estiver usando Tailwind
+    autoprefixer: {},   // essencial para gerar estilos compatíveis
+  }
+}
